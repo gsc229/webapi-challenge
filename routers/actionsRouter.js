21afe3f43e84aa1fa17728cd59d414e04a733c7f
Greg <gsc229@gmail.com>
2019-11-01 13:04:43 -0400

    MVP

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -13,6 +13,31 @@ router.get('/', (req, res) => {
 })
 
 
+router.post('/', (req, res) => {
+  Actions.insert(req.body)
+    .then(newAction => {
+      res.status(201).json(newAction);
+    })
+    .catch(err => { res.status(500).json({ errorMessage: "Something went wrong with your post" }) })
+})
+
+router.put('/:id', (req, res) => {
+  Actions.update(req.body.id, req.body)
+    .then(updatedProj => {
+      res.status(200).json(updatedProj);
+    })
+    .catch(err => res.status(500).json({ errorMessage: "Something went wrong with your update" }))
+
+})
+
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.params.id)
+    .then(info => res.status(200).json(info))
+    .catch(err => res.status(500).json({ errorMessage: "Something went wrong trying to delete that project" }))
+})
+
+
+
 
 
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -12,6 +12,15 @@ router.get('/', (req, res) => {
 
 })
 
+router.get('/:id', (req, res) => {
+  Projects.get(req.params.id)
+    .then(proj => {
+      console.log("Projects router: projs", proj);
+      res.status(200).json(proj);
+    })
+
+})
+
 router.post('/', (req, res) => {
   Projects.insert(req.body)
     .then(newProject => {
@@ -19,6 +28,22 @@ router.post('/', (req, res) => {
     })
     .catch(err => { res.status(500).json({ errorMessage: "Something went wrong with your post" }) })
 })
+
+router.put('/:id', (req, res) => {
+  Projects.update(req.body.id, req.body)
+    .then(updatedProj => {
+      res.status(200).json(updatedProj);
+    })
+    .catch(err => res.status(500).json({ errorMessage: "Something went wrong with your update" }))
+
+})
+
+router.delete('/:id', (req, res) => {
+  Projects.remove(req.params.id)
+    .then(info => res.status(200).json(info))
+    .catch(err => res.status(500).json({ errorMessage: "Something went wrong trying to delete that project" }))
+})
+
 
 
 module.exports = router;
